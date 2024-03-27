29cc891387d578ee76d8a8636aee071ea2887a36
tenpei999 <tenpei999@gmail.com>
2024-03-27 23:18:19 +0900

    merged

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9892ee125aa6e32026f0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5ae89368055b61fb1126');
--- build/index.js
+++ build/index.js
@@ -2242,24 +2242,36 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _processWeatherData__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./processWeatherData */ "./src/weatherDate/processWeatherData.js");
 
 
+
+/**
+ * Orchestrates the fetching and processing of weather data for a specific city and updates
+ * state hooks for today's, tomorrow's, and the weekly weather forecasts.
+ * 
+ * @param {string} cityurl - The URL to fetch weather data for a specific city.
+ * @param {Function} setTodayWeather - State setter function for today's weather.
+ * @param {Function} setTomorrowWeather - State setter function for tomorrow's weather.
+ * @param {Function} setWeeklyWeather - State setter function for the weekly weather forecast.
+ * @param {boolean} addBreak - Optional parameter indicating whether to add a break in the processing of weather data.
+ */
 async function mainWeatherLogic(cityurl, setTodayWeather, setTomorrowWeather, setWeeklyWeather, addBreak = false) {
   try {
-    // 1. 天気データを取得
+    // Fetch raw weather data from the provided city URL.
     const rawData = await (0,_fetchWeatherData__WEBPACK_IMPORTED_MODULE_0__.fetchWeatherData)(cityurl);
-
-    // 2. 天気データを処理（加工）
+    // Process the raw weather data, optionally adding breaks based on the addBreak parameter.
     const processedData = await (0,_processWeatherData__WEBPACK_IMPORTED_MODULE_1__["default"])(rawData, addBreak);
 
-    // 3. UIを更新するためのデータをセット
+    // Update the state for today's weather using the first item from the processed daily data.
     setTodayWeather(processedData.dailyData[0]);
+    // Update the state for tomorrow's weather using the second item from the processed daily data.
     setTomorrowWeather(processedData.dailyData[1]);
-    setWeeklyWeather(processedData.dailyData.slice(2)); // 2日目以降のデータを週間天気として設定
+    // Update the state for the weekly weather forecast using the rest of the items from the processed daily data.
+    setWeeklyWeather(processedData.dailyData.slice(2));
   } catch (error) {
+    // Log any errors that occur during the fetching or processing of weather data.
     console.error('Error in weatherObject function:', error);
-    // 必要に応じて、ここでエラーハンドリングを行う
   }
 }
-
+;
 
 
 /***/ }),
